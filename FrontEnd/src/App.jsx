a08f56087e9a29ab92953e5db37c28495787331a
adarsh <adarshdeshmukh00000@gmail.com>
2026-09-13 03:42:15 +0530

    test

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className="flex max-h-screen items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-3xl font-bold">Vite + React + Taildxdvkjzxvwind CShipig zdfvbfkznfkvbS</h1>
+      <h1 className="text-3xl font-bolder">Vite + React + Taildxdvkjzxvwind CShipig zdfvbfkznfkvbS</h1>
       <h1>gjjtyty</h1>
     </div>
     
