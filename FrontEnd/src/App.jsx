bab24e2eba325fd5d781bea5f42f7c0c6a177aa7
adarsh <adarshdeshmukh00000@gmail.com>
2026-09-13 03:28:46 +0530

    test

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold">Vite + React + Tailwind CSS</h1>
+    </div>
+  )
+}
+
+export default App
